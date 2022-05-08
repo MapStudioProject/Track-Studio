6952fa2efadd397075eac7b92e3bb6cb479c7c2c
KillzXGaming <millernathan35@gmail.com>
2022-05-08 18:44:43 -0400

    Skip transforming models as they are already transformed by the node transform

--- Plugins/CafeLibrary/Bfres/Editing/ModelConversion/BfresModelImporter.cs
+++ Plugins/CafeLibrary/Bfres/Editing/ModelConversion/BfresModelImporter.cs
@@ -161,6 +161,11 @@ namespace CafeLibrary.ModelConversion
                 }
             }
 
+            //Default the root to identity atm. Course maps may keep bone transforms which will break the model transform
+            fmdl.Skeleton.Bones[0].Position = new Vector3F(0, 0, 0);
+            fmdl.Skeleton.Bones[0].Scale = new Vector3F(1, 1, 1);
+            fmdl.Skeleton.Bones[0].Rotation = new Vector4F(0, 0, 0, 1);
+
             if (fmdl.Skeleton.Bones.Count == 0) {
                 fmdl.Skeleton.Bones.Add("Root", new Bone() { Name = "Root" });
             }
@@ -182,52 +187,54 @@ namespace CafeLibrary.ModelConversion
 
                 //Set the skin count
                 uint vertexSkinCount = 0;
+                skinCounts[sindex++] = vertexSkinCount;
 
-                try
-                {
-                    bool rigidBind = false;
-                    //Check if the bone list has only 1 bone or less. If so it uses rigid binding
-                    var riggedBones = mesh.Vertices.SelectMany(x => x.Envelope.Weights?.Select(x => x.BoneName)).Distinct().ToList();
-                     if (riggedBones == null || riggedBones.Count <= 1)
-                        rigidBind = true;
+                /*
+                                try
+                                {
+                                    bool rigidBind = false;
+                                    //Check if the bone list has only 1 bone or less. If so it uses rigid binding
+                                    var riggedBones = mesh.Vertices.SelectMany(x => x.Envelope.Weights?.Select(x => x.BoneName)).Distinct().ToList();
+                                     if (riggedBones == null || riggedBones.Count <= 1)
+                                        rigidBind = true;
 
-                    if (!rigidBind)
-                        CalculateSkinCount(mesh.Vertices);
+                                    if (!rigidBind)
+                                        CalculateSkinCount(mesh.Vertices);
 
-                    //Todo. This basically reimports meshes with the original skin count to target as
-                    /*    if (importSettings.Meshes.Any(x => x.Name == mesh.Name))
-                        {
-                            var meshSettings = importSettings.Meshes.FirstOrDefault(x => x.Name == mesh.Name);
-                            vertexSkinCount = (uint)meshSettings.SkinCount;
-                        }*/
+                                    //Todo. This basically reimports meshes with the original skin count to target as
+                                    /*    if (importSettings.Meshes.Any(x => x.Name == mesh.Name))
+                                        {
+                                            var meshSettings = importSettings.Meshes.FirstOrDefault(x => x.Name == mesh.Name);
+                                            vertexSkinCount = (uint)meshSettings.SkinCount;
+                                        }
 
-                    //Set the skin count for each mesh. This is either calculated or applied via mesh meta info
-                    skinCounts[sindex++] = vertexSkinCount;
+                                    //Set the skin count for each mesh. This is either calculated or applied via mesh meta info
+                                    skinCounts[sindex++] = vertexSkinCount;
 
-                    //Transform rigid bindings into worldspace
-                    if (rigidBind && riggedBones?.Count == 1)
-                    {
-                        //For rigid bind types we will fully transform the model into worldspace 
-                        var bn = model.Skeleton.BreathFirstOrder().Where(x => x.Name == riggedBones[0]).FirstOrDefault();
-                        if (bn != null)
-                            mesh.TransformVertices(bn.WorldTransform);
+                                    //Transform rigid bindings into worldspace
+                                    if (rigidBind && riggedBones?.Count == 1)
+                                    {
+                                        //For rigid bind types we will fully transform the model into worldspace 
+                                        var bn = model.Skeleton.BreathFirstOrder().Where(x => x.Name == riggedBones[0]).FirstOrDefault();
+                                        if (bn != null)
+                                            mesh.TransformVertices(bn.WorldTransform);
 
-                        //Set the bone into identity as we want these to be applied
-                        var bfresBone = fmdl.Skeleton.Bones.Values.Where(x => x.Name == riggedBones[0]).FirstOrDefault();
-                        if (bfresBone != null)
-                        {
-                            bfresBone.Position = new Vector3F(0, 0, 0);
-                            bfresBone.Rotation = new Vector4F(0, 0, 0, 1);
-                            bfresBone.Scale = new Vector3F(1, 1, 1);
-                        }
-                        continue;
-                    }
-                }
-                catch
-                {
+                                        //Set the bone into identity as we want these to be applied
+                                        var bfresBone = fmdl.Skeleton.Bones.Values.Where(x => x.Name == riggedBones[0]).FirstOrDefault();
+                                        if (bfresBone != null)
+                                        {
+                                            bfresBone.Position = new Vector3F(0, 0, 0);
+                                            bfresBone.Rotation = new Vector4F(0, 0, 0, 1);
+                                            bfresBone.Scale = new Vector3F(1, 1, 1);
+                                        }
+                                        continue;
+                                    }
+                                }
+                                catch
+                                {
 
-                }
-
+                                }
+                                */
                 foreach (var vertex in mesh.Vertices)
                 {
                     foreach (var weight in vertex.Envelope.Weights)
